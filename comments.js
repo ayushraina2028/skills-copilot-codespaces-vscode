0669cb820619840e095d4b115648474a6c68249c
Ayush Raina <ayushraina@iisc.ac.in>
2023-07-28 20:32:24 +0000

    xyz

--- comments.js
+++ comments.js
@@ -1,61 +1,29 @@
 // create web server
-// 1. npm init -y
-// 2. npm install express --save
-// 3. npm install body-parser --save
-// 4. npm install express-session --save
-// 5. npm install multer --save
-// 6. npm install mysql --save
-// 7. npm install fs --save
-// 8. npm install ejs --save
-// 9. npm install nodemailer --save
-// 10. npm install nodemailer-smtp-transport --save
-// 11. npm install moment --save
-// 12. npm install multer --save
-// 13. npm install multer-s3 --save
-// 14. npm install aws-sdk --save
-// 15. npm install crypto --save
-// 16. npm install nodemailer-smtp-transport --save
-// 17. npm install nodemailer --save
-// 18. npm install multer --save
-// 19. npm install multer-s3 --save
-// 20. npm install aws-sdk --save
-// 21. npm install crypto --save
-// 22. npm install fs --save
-// 23. npm install ejs --save
-// 24. npm install nodemailer --save
-// 25. npm install nodemailer-smtp-transport --save
-// 26. npm install moment --save
-// 27. npm install multer --save
-// 28. npm install multer-s3 --save
-// 29. npm install aws-sdk --save
-// 30. npm install crypto --save
-// 31. npm install fs --save
-// 32. npm install ejs --save
-// 33. npm install nodemailer --save
-// 34. npm install nodemailer-smtp-transport --save
-// 35. npm install moment --save
-// 36. npm install multer --save
-// 37. npm install multer-s3 --save
-// 38. npm install aws-sdk --save
-// 39. npm install crypto --save
-// 40. npm install fs --save
-// 41. npm install ejs --save
-// 42. npm install nodemailer --save
-// 43. npm install nodemailer-smtp-transport --save
-// 44. npm install moment --save
-// 45. npm install multer --save
-//// 1. load modules
+// 1. create web server
+// 2. create router
+// 3. create router handler
+// 4. create router middleware
+// 5. create router handler function
+// 6. create router middleware function
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var multer = require('multer');
-var mysql = require('mysql');
+// 1. create web server
+const express = require('express');
+const app = express();
 
-var fs = require('fs');
-var ejs = require('ejs');
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
-var moment = require('moment');
+// 2. create router
+const comments = require('./routes/comments');
 
-var multerS3 = require('multer-s3');
+// 3. create router handler
+app.use('/comments', comments);
+
+// 4. create router middleware
+app.use((req, res, next) => {
+  res.status(404).send('404 Page Not Found');
+});
+
+// 5. create router handler function
+// 6. create router middleware function
+
+module.exports = app;
+
+// Path: comments.js
