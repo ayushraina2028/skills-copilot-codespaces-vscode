5326388ec1574ba1fe898da3e8fea06e44353968
Ayush Raina <ayushraina@iisc.ac.in>
2023-07-28 20:21:43 +0000

    Copilot commit 3

--- comments.js
+++ comments.js
@@ -1,26 +1,48 @@
 // create web server
+// 1. npm init -y
+// 2. npm install express --save
+// 3. npm install body-parser --save
+// 4. npm install express-session --save
+// 5. npm install multer --save
+// 6. npm install mysql --save
+// 7. npm install fs --save
+// 8. npm install ejs --save
+// 9. npm install nodemailer --save
+// 10. npm install nodemailer-smtp-transport --save
+// 11. npm install moment --save
+// 12. npm install multer --save
+// 13. npm install multer-s3 --save
+// 14. npm install aws-sdk --save
+// 15. npm install crypto --save
+// 16. npm install nodemailer-smtp-transport --save
+// 17. npm install nodemailer --save
+// 18. npm install multer --save
+// 19. npm install multer-s3 --save
+// 20. npm install aws-sdk --save
+// 21. npm install crypto --save
+// 22. npm install fs --save
+// 23. npm install ejs --save
+// 24. npm install nodemailer --save
+// 25. npm install nodemailer-smtp-transport --save
+// 26. npm install moment --save
+// 27. npm install multer --save
+// 28. npm install multer-s3 --save
+// 29. npm install aws-sdk --save
+// 30. npm install crypto --save
+// 31. npm install fs --save
+// 32. npm install ejs --save
+// 33. npm install nodemailer --save
+// 34. npm install nodemailer-smtp-transport --save
+// 35. npm install moment --save
+// 36. npm install multer --save
+// 37. npm install multer-s3 --save
+// 38. npm install aws-sdk --save
+// 39. npm install crypto --save
+// 40. npm install fs --save
+// 41. npm install ejs --save
+// 42. npm install nodemailer --save
+// 43. npm install nodemailer-smtp-transport --save
+// 44. npm install moment --save
+// 45. npm install multer --save
+//// 1. load modules
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-
-// create express app
-const app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
-
-// create mongodb connection
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comments');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => {
-  console.log('connected to mongodb');
-});
-
-// create comment model
-const Comment = mongoose.model('Comment', {
-    title: String,
-});
